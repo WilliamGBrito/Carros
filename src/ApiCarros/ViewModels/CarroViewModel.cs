f269cf9d6ce76183823b1ff75e91e90e33cdb6a4
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-17 18:09:00 -0300

    feature/Adicionado_ViewModel

---
+++ src/ApiCarros/ViewModels/CarroViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCarros.ViewModels
+{
+    public class CarroViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string? Marca { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string? Modelo { get; set; }
+    }
+}
