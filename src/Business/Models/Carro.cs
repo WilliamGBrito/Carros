af036a4497acc1b9112335df3be4ef585ba6ada0
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-17 15:53:35 -0300

    feature/Adicionado_as_models_do_proejeto

---
+++ src/Business/Models/Carro.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Models
+{
+    public class Carro : Entity
+    {
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+    }
+}
---
+++ src/Business/Models/Validations/CarroValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Business.Models.Validations
+{
+    public class CarroValidation : AbstractValidator<Carro>
+    {
+        public CarroValidation()
+        {
+            RuleFor(c => c.Marca)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 30).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(c => c.Modelo)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 30).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
+    }
+}
